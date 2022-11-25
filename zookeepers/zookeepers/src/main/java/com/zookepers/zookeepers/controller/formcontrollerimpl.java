d5044d20d2eaa5bca4410a607a4edba774336a81
onechanho <chanho003@naver.com>
2022-11-25 14:24:27 +0900

    add: zookeepers 툴 변경건

---
+++ zookeepers/zookeepers/src/main/java/com/zookepers/zookeepers/controller/formcontrollerimpl.java
@@ -0,0 +1,51 @@
+package com.zookepers.zookeepers.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class formcontrollerimpl implements formcontroller{
+    
+    	
+	@GetMapping("/")
+	public String main() {
+		
+		return "index";
+	}
+	
+	@GetMapping("/login.html")
+	public String login() {
+		
+		return "login";
+	}
+	
+	@GetMapping("/join.html")
+	public String join() {
+		return "join";
+	}
+	
+	@GetMapping("/Board.html")
+	public String board() {
+		return "Board";
+	}
+	
+	@GetMapping("/Board_detail.html")
+	public String board_detail() {
+		return "Board_detail";
+	}
+	
+	@GetMapping("/TBoard.html")
+	public String tboard() {
+		return "TBoard";
+	}
+	
+	@GetMapping("/TBoard_detail.html")
+	public String tboard_detail() {
+		return "TBoard_detail";
+	}
+	
+	@GetMapping("/Mypage.html")
+	public String mypage() {
+		return "mypage";
+	}
+}
