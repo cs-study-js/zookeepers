2bdf4bddbd08a34e493c3b2775ccf0f592a010c3
onechanho <chanho003@naver.com>
2022-11-29 20:18:11 +0900

    style : TJBD-6 피드백에 따른  수정건

--- zookeepers/zookeepers/src/main/java/com/zookepers/zookeepers/controller/formcontrollerimpl.java
+++ zookeepers/zookeepers/src/main/java/com/zookepers/zookeepers/controller/formcontrollerimpl.java
@@ -13,38 +13,38 @@ public class formcontrollerimpl implements formcontroller{
 		return "index";
 	}
 	
-	@GetMapping("/login.html")
+	@GetMapping("/login.do")
 	public String login() {
 		
 		return "login";
 	}
 	
-	@GetMapping("/join.html")
+	@GetMapping("/join.do")
 	public String join() {
 		return "join";
 	}
 	
-	@GetMapping("/Board.html")
+	@GetMapping("/Board.do")
 	public String board() {
 		return "Board";
 	}
 	
-	@GetMapping("/Board_detail.html")
+	@GetMapping("/Board_detail.do")
 	public String board_detail() {
 		return "Board_detail";
 	}
 	
-	@GetMapping("/TBoard.html")
+	@GetMapping("/TBoard.do")
 	public String tboard() {
 		return "TBoard";
 	}
 	
-	@GetMapping("/TBoard_detail.html")
+	@GetMapping("/TBoard_detail.do")
 	public String tboard_detail() {
 		return "TBoard_detail";
 	}
 	
-	@GetMapping("/Mypage.html")
+	@GetMapping("/Mypage.do")
 	public String mypage() {
 		return "mypage";
 	}
